8141ffd63b1c467fccdf1e1a15f1db032f21c5fb
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-09-06 14:34:46 -0700

    Delete selectingElementsJS.js

--- selectingElementsJS.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('num1').textContent += 'I am creating context without inputting it into directly into the HTML page.';
-document.getElementById('num0').textContent += 'Wicked cool title.';
-console.log(document);
