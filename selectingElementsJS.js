ee72d3474254f9d08afe87fe0e089095144f93ea
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-09-06 14:35:29 -0700

    selecting elements JS
    selecting elements JS

---
+++ selectingElementsJS.js
@@ -0,0 +1,3 @@
+document.getElementById('num1').textContent += 'I am creating context without inputting it into directly into the HTML page.';
+document.getElementById('num0').textContent += 'Wicked cool title.';
+console.log(document);
