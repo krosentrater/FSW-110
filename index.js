5ffb284583be55b0d66a24fb279acbde8634b15a
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-09-06 18:26:20 -0700

    Creating basic web page with JS

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
-const h1Tag = document.createElement('h1');
-const h1Text = document.createTextNode('Welcome to my JS site!'); 
+const navBar = document.querySelector('#body');
+let nav = document.createElement('a');
+nav.setAttribute('href', 'file:///C:/Users/Kyle/Desktop/School/Classes/FSW-110/Week2/Create%20Basic%20Website%20w_JS/index.html');
+nav.textContent = 'Home | ';
+navBar.appendChild(nav);
+
+let nav2 = document.createElement('a');
+nav2.setAttribute('href', 'https://www.w3schools.com/');
+nav2.textContent = 'Sources | ';
+navBar.appendChild(nav2);
+
+let nav3 = document.createElement('a');
+nav3.setAttribute('href', '');
+nav3.textContent = 'Etc... | ';
+navBar.appendChild(nav3);
+
+let h1Tag = document.createElement('h1');
+let h1Text = document.createTextNode('H1 tag created with JavaScript');
 h1Tag.appendChild(h1Text);
 
-let heading = document.getElementById('num1'); 
-heading.appendChild(h1Tag);
-//adding h1
+const h1 = document.querySelector('#body');
+h1.appendChild(h1Tag);
+// Create H1 Tag Done
 
-const pTag = document.createElement('p');
-const pText = document.createTextNode('All of this was created with JavaScrip:');
+let pTag = document.createElement('p');
+let pText = document.createTextNode('This paragraph has been brought to you by JavaScript.');
 pTag.appendChild(pText);
 
-let paragraph = document.querySelector('#num2');
-paragraph.appendChild(pTag);
-//adding p
+const p = document.querySelector('#body');
+p.appendChild(pTag);
+// Create P Tag Done
 
-const myList = document.createElement('li');
-const liText = document.createTextNode('Create new element.');
-myList.appendChild(liText);
+let li1 = document.createElement('li');
+let li1Text = document.createTextNode('First Item on List');
+li1.appendChild(li1Text);
 
-let li1 = document.getElementById('list');
-li1.appendChild(myList);
-//li1
-const myList2 = document.createElement('li');
-const liText2 = document.createTextNode('Create text for new element.');
-myList2.appendChild(liText2);
+const li1Tag = document.querySelector('#body');
+li1Tag.appendChild(li1);
 
-let li2 = document.getElementById('list');
-li2.appendChild(myList2);
-//li2
-const myList3 = document.createElement('li');
-const liText3 = document.createTextNode('Append text for the new element to the new element.');
-myList3.appendChild(liText3);
+let li2 = document.createElement('li');
+let li2Text = document.createTextNode('Second Item on List');
+li2.appendChild(li2Text);
 
-let li3 = document.getElementById('list');
-li3.appendChild(myList3);
-//li3
-console.log(document);
+const li2Tag = document.querySelector('#body');
+li2Tag.appendChild(li2);
+
+let li3 = document.createElement('li');
+let li3Text = document.createTextNode('Third Item on List');
+li3.appendChild(li3Text);
+
+const li3Tag = document.querySelector('#body');
+li3Tag.appendChild(li3); 
+// Create List Done
+
+let footer = document.createElement('footer');
+let footerText = document.createTextNode('Website Design: Kyle Rosetrater');
+footer.appendChild(footerText);
+
+const footerTag = document.querySelector('#body');
+footerTag.appendChild(footer);
+// Create Footer Done
