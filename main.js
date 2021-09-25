6938765db60d39a1bb35861f75dace15c3c0c584
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-09-24 20:13:48 -0700

    JavaScript Calculator
    .html file, .js file, .css file for javascript calculator

--- main.js
+++ main.js
@@ -1,64 +1,26 @@
-const square = document.querySelector('.square');
-
-window.onload = function onLoad(){
-    document.querySelector('.square').style.backgroundColor = 'black';
+function add() {
+    let num1 = document.getElementById('add-one').value;
+    let num2 = document.getElementById('add-two').value;
+    let result;
+    result = (Number(num1) + Number(num2));
+    document.getElementById('add-result').value = result;
 };
-//onload event complete
-function mouseOver(){
-    document.querySelector('.square').style.backgroundColor = 'green';
+document.getElementById('add').addEventListener('click', add);
+
+function sub () {
+    let num1 = document.getElementById('sub-one').value;
+    let num2 = document.getElementById('sub-two').value;
+    let result;
+    result = (Number(num1) - Number(num2));
+    document.getElementById('sub-result').value = result;
 };
-square.addEventListener('mouseover', mouseOver);
-//mouseover event complete
+document.getElementById('sub').addEventListener('click', sub);
 
-function holdClick (){
-    document.querySelector('.square').style.backgroundColor = 'yellow';
-    setTimeout(function(){
-
-    }, 500);
+function mult () {
+    let num1 = document.getElementById('mult-one').value;
+    let num2 = document.getElementById('mult-two').value;
+    let result;
+    result = (Number(num1) * Number(num2));
+    document.getElementById('mult-result').value = result;
 };
-square.addEventListener('mousedown', holdClick);
-//mousedown event complete
-
-function letGoOfMouseButton(){
-    document.querySelector('.square').style.backgroundColor = 'blue';
-};
-square.addEventListener('mouseup', letGoOfMouseButton);
-//mouseup event complete
-
-function doubleClick(){
-    document.querySelector('.square').style.backgroundColor = 'red';
-};
-square.addEventListener('dblclick', doubleClick);
-//dblclick event complete
-
-function mouseScroll(){
-    document.querySelector('.square').style.backgroundColor = 'purple';
-};
-square.addEventListener('wheel', mouseScroll);
-//mouseScroll event complete
-
-document.addEventListener('keypress', function(event){
-    let key = event.key;
-    if (key == 'r'){
-        document.querySelector('.square').style.backgroundColor = 'red';
-    }
-    else if (key == 'b'){
-        document.querySelector('.square').style.backgroundColor = 'black';
-    }
-    else if (key == 'g'){
-        document.querySelector('.square').style.backgroundColor = 'green';
-    }
-    else if (key == 'w'){
-        document.querySelector('.square').style.backgroundColor = 'white';
-    }
-    else if (key == 'y'){
-        document.querySelector('.square').style.backgroundColor = 'yellow';
-    }
-    else if (key == 'b'){
-        document.querySelector('.square').style.backgroundColor = 'blue';
-    }
-    else if (key == 'p'){
-        document.querySelector('.square').style.backgroundColor = 'purple';
-    }
-});
-//key board key events done
+document.getElementById('mult').addEventListener('click', mult);
